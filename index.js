0f3ace7bec0de4b874bc4fc48739a6a9062306b1
João Paulo Gonçalves Sanches <jpfaipa@gmail.com>
2019-04-13 23:29:01 -0300

    Fixing path public

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
 // use, toda requicao passa aqui
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.urlencoded({
     extended: true
 }))

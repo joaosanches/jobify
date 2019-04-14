15f73207207b828a0aca7e85670e17ba88eaba58
João Paulo Gonçalves Sanches <jpfaipa@gmail.com>
2019-04-13 23:39:06 -0300

    Bloqueando acesso ao admin fora do localhost

--- index.js
+++ index.js
@@ -8,8 +8,15 @@ const dbConnection = sqlite.open(path.resolve(__dirname, 'banco.sqlite'), { Prom
 
 const bodyParser = require('body-parser')
 
-
 const port = process.env.PORT | 3000
+
+app.use('/admin', (req, res, next) => {
+    if (req.hostname === 'localhost') {
+        next()
+    } else {
+        res.send('Not allowed')
+    }
+})
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
